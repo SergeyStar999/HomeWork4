cc2f1d4c2ce60be2575da669c71a47a3299e2e09
Сергей <sergeystarod@mail.ru>
2022-08-15 17:37:01 +0300

    Мой второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(" " + i);
+        }
+
+        System.out.println();
+
+        for (int b = 10; b > 0; b--) {
+            System.out.print(" " + b);
+        }
+
+        System.out.println();
+
+        // Задание 2
+
+        for (int a = 2; a <= 31; a = a + 7) {
+            System.out.println("Сегодня пятница, " + a + "-е число. Необходимо подготовить отчет");
+        }
+
+        // Задание 3
+
+        int start = 1822;
+        for (int year = 1822; year <= 2122; year++) {
+            if (year %79 == 0) {
+                System.out.println(year);
+            }
+        }
     }
 }
